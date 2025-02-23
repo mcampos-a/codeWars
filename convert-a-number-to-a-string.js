772f96e6842e2a7714e89c425c9c6ad541f90db4
Max Campos <mcampos2a@gmail.com>
2025-02-23 19:28:56 +0100

    pushing daily kata practising fundamentals and strings

---
+++ convert-a-number-to-a-string.js
@@ -0,0 +1,29 @@
+//8 Kyu
+//Convert a Number to a String!
+//Fundamentals, strings
+
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+
+
+
+//Solution I
+
+function numberToString(num) {
+//we can use the .toString() method here 
+    return num.toString()
+  }
+
+
+//Parameters
+//num-> and we want an a string out of it. 
+//no empty num, no null, no symbols, no funny bizz
+
+//Return
+//a string from the num provided as a paramater to our function
+
+//Example
+//console.log(numberToString(34), '34')
+
