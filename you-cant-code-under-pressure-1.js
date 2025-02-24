ac07abb0eb12094227257572d3d9cca5b855ad93
Max Campos <mcampos2a@gmail.com>
2025-02-24 11:26:11 +0100

    commiting daily kata playing with integers

---
+++ you-cant-code-under-pressure-1.js
@@ -0,0 +1,28 @@
+//8 Kyu
+//You Can't Code Under Pressure #1
+//Fundamentals
+
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+
+
+
+//Solution I
+
+function doubleInteger(i) {
+  //take incoming num i and mupltiply by 2 to double
+  //return it
+  return i*2
+}
+
+//Parameters
+//num-> and num going out. Can be empty, not null, no symbols, no funny bizz.
+//
+//Returns
+//num*2, a doubling of the incoming num
+
+//examples
+//console.log(doubleInteger(2), 4)
+//console.log(doubleInteger(10), 20)
