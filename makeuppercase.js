923f3d1918ec19a8b45fa04ecba5045fa9cd9a2c
Max Campos <mcampos2a@gmail.com>
2025-03-25 13:12:16 +0100

    committing daily kata

---
+++ makeuppercase.js
@@ -0,0 +1,24 @@
+//8 Kyu
+//MakeUpperCase
+//Fundamentals, strings
+
+// Write a function which converts the input string to uppercase.
+
+
+//Solution I
+
+function makeUpperCase(str) {
+  //use the .toUpperCase() method on the incoming str and return it
+  return str.toUpperCase()
+}
+
+
+
+//Parameters
+//str-> can be empty, not null, only str coming in
+
+//Return
+//str-> all elements toUpperCase()
+
+//Example
+//console.log(makeUpperCase('Today'), 'TODAY')
