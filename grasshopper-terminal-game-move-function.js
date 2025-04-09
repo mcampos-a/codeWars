b0b2dbdee613a883c205ec10efd0aeb418b8e3b8
Max Campos <mcampos2a@gmail.com>
2025-04-09 11:31:34 -0700

    commiting daily kata

---
+++ grasshopper-terminal-game-move-function.js
@@ -0,0 +1,36 @@
+//8 Kyu
+//Third Angle of a Triangle
+//Fundamentals
+
+// Terminal game move function
+// In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+// Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+// Example:
+// move(3, 6) should equal 15
+
+
+
+//Solution I
+
+function move (position, roll) {
+  // return the new position
+}
+  
+
+//Parameters
+//num1, num2 -> num1 = starting point, num2 = next move (1d6 * 2), noever empty nums, never null, only nums. dice is a 6 sided dice
+
+//return
+//num -> new position = num1 + (num2 * 2)
+
+//Example
+//console.log(move(2, 6) 14)
+
+
+
+
+
+
+
