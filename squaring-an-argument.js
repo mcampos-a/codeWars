4b198b5a709c3ef400282c9146962904f167665e
Max Campos <mcampos2a@gmail.com>
2025-04-14 14:34:30 -0700

    committing daily kata

---
+++ squaring-an-argument.js
@@ -0,0 +1,20 @@
+//8 Kyu
+//Function 2 - squaring an argument
+//Fundamentals
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+function square(num){
+  return Math.pow(num,2)
+}
+
+//Parameters
+
+
+
+
+
+
+
+
+
