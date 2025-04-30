f8c0f1da06f1abe797956c4a6e23bd5c229cf88c
Max Campos <mcampos2a@gmail.com>
2025-04-29 21:48:42 -0700

    committing daily kata

---
+++ lost-without-a-map.js
@@ -0,0 +1,39 @@
+//8 Kyu
+//Lost Without a Map
+//Fundamentals, Arrays
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+//Solution I
+
+function maps(x){
+//map through the x array and make sure each element is num+num and return it. 
+  return x.map(el => el+el)
+}
+
+//Parameters
+//arr -> nums, it can be empty, it wont be null, and we will always get an arr
+
+//Return
+//arr -> each element num = num + num.
+
+//Example
+//console.log(maps([1, 2, 3]), [2, 4, 6])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
