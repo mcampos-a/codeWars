7fb8fb9199864134737d355687dca685620f4dae
Max Campos <mcampos2a@gmail.com>
2025-05-12 09:58:12 +0200

    committing daily kata

---
+++ hex-to-decimal.js
@@ -0,0 +1,16 @@
+//8 Kyu
+//Hex to Decimal
+//Fundamentals
+
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+
+
+//Solution I
+
+function hexToDec(hexString){
+//Use the parseInt method to convert from hex to number
+return parseInt(hexString, 16)
+}
+    
+
