5bf1a749d949fa6e4a04127268fd1cdfd3f2db03
Max Campos <mcampos2a@gmail.com>
2025-07-20 22:19:36 -0500

    commit daily kata ons trings

---
+++ combine-strings.js
@@ -0,0 +1,18 @@
+//8 Kyu
+//Combine Strings
+//Fundamentals, strings
+
+// Combine strings function
+// Create a function named combineNames/combine_names/CombineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// With "James" as the first name and "Stevens" as the last name should return "James Stevens"
+
+
+
+function combineNames(firstName,lastName){
+  return `${firstName} ${lastName}`
+}
+
+
