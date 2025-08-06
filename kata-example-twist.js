6522b3ab60da00bc146fa90650118dc1d7a1b76b
Max Campos <mcampos2a@gmail.com>
2025-08-06 13:13:51 -0500

    commit daily kata

---
+++ kata-example-twist.js
@@ -0,0 +1,18 @@
+//8 Kyu
+//Kata Example Twist
+//Fundamentals, arrays
+
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+// Add the value "codewars" to the array websites 1,000 times.
+
+
+
+//Solution
+
+// add the value "codewars" to the websites array 1,000 times
+let websites = []
+
+while(websites.length <= 10000){
+  websites.push("codewars")
+}
