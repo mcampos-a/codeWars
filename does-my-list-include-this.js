e809c70d01c185393fcd9875300adaa17dbed499
Max Campos <mcampos2a@gmail.com>
2025-08-07 11:30:56 -0500

    commit daily kata

---
+++ does-my-list-include-this.js
@@ -0,0 +1,13 @@
+//8 Kyu
+//Does My List Include This?
+//Fundamentals
+
+// Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+
+
+//Solution
+
+function includes(arr, item){
+  return arr.include(item)
+}
