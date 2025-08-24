ac993f13a7e3e8a91d93e225a84e2479ba50c195
Max Campos <mcampos2a@gmail.com>
2025-08-23 17:17:22 -0700

    commit daily kata

---
+++ leonadro-dicaprio-oscars.js
@@ -0,0 +1,33 @@
+//7 Kyu
+//Leonardo Dicaprio and Oscars
+//Fundamentals, Strings
+
+// You have to write a function that describe Leo:
+
+// function leo(oscar) {
+
+// }
+// if oscar was (integer) 88, you have to return "Leo finally won the oscar! Leo is happy".
+// if oscar was 86, you have to return "Not even for Wolf of wallstreet?!"
+// if it was not 88 or 86 (and below 88) you should return "When will you give Leo an Oscar?"
+// if it was over 88 you should return "Leo got one already!"
+
+
+
+//Solution
+
+function leo(oscar){
+    if(oscar != 88 && oscar != 86 && oscar < 88){
+        return "When will you give Leo an Oscar?"
+    }else if(oscar > 88){
+        return "Leo got one already!"
+    }else if(oscar === 88){
+        return "Leo finally won the oscar! Leo is happy"
+    }else if(oscar === 86){
+        return "Not even for Wolf of wallstreet?!"
+    }
+}
+
+//num -> the oscar number, never empty, not null or undefined, always a num coming
+
+
