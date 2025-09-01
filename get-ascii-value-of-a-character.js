5434a75deefb990d2954f5963906ce4a23c55cd3
Max Campos <mcampos2a@gmail.com>
2025-09-01 11:58:12 -0700

    commit daily kata

---
+++ get-ascii-value-of-a-character.js
@@ -0,0 +1,19 @@
+//8 Kyu
+//Get ASCII Value of Character
+//Fundamentals
+
+// Get ASCII value of a character.
+
+// For the ASCII table you can refer to http://www.asciitable.com/
+
+
+
+//Solution
+
+function getASCII(c){
+//use the .charCodeAt() method to pull the value of the character input and return it
+  return c.charCodeAt()
+}
+
+
+
