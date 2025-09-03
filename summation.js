c872e92d7f8538436933cc25473c471f25ccdc1c
Max Campos <mcampos2a@gmail.com>
2025-09-03 10:28:18 -0700

    commit daily kata

---
+++ summation.js
@@ -0,0 +1,30 @@
+//8 Kyu
+//Summation
+//Fundamentals, mathematics
+
+// Summation
+// Write a program that finds the summation of every number from 1 to num (both inclusive). The number will always be a positive integer greater than 0. Your function only needs to return the result, what is shown between parentheses in the example below is how you reach that result and it's not part of it, see the sample tests.
+
+// For example (Input -> Output):
+
+// 2 -> 3 (1 + 2)
+// 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+
+
+//Solution
+
+var summation = function (num) {
+//set up a variable to store sum
+  let sum = 0
+//loop from 1 to number input
+for(let i=1; i<=num; i++){
+//add the current number
+    sum+=i
+  }
+//return sum
+  return sum
+}
+
+
+
