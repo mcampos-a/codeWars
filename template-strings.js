039dafef459cb2f1cc0d2456145d433772847ed2
Max Campos <mcampos2a@gmail.com>
2025-09-04 17:45:28 -0700

    commit daily kata

---
+++ template-strings.js
@@ -0,0 +1,23 @@
+//8 Kyu
+//Template Strings
+//Fundamentals, strings
+
+// Template Strings
+// Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+// Task
+// Your task is to return the correct string using the Template String Feature.
+// Input
+// Two Strings, no validation is needed.
+// Output
+// You must output a string containing the two strings with the word ```' are '```
+
+
+
+//Solution
+
+var templateStrings = function(noun, adjective) {
+  return `${noun} are ${adjective}`
+}
+
+
+
