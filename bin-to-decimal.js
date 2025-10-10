8fbbf46177c7cc8f421f1324ddcbdd146d40dc50
Max Campos <mcampos2a@gmail.com>
2025-10-09 22:03:38 -0600

    commit daily kata

---
+++ bin-to-decimal.js
@@ -0,0 +1,19 @@
+//8 Kyu
+//Bin to Decimal
+//Fundamentals, binary
+
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+
+//Solution
+
+function binToDec(bin) {
+  return parseInt(bin, 2)
+}
+
+
+
+
+
+
+
