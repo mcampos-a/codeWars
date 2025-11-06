cdd5808884f81f59f6e22c77a23ec524ca648bd9
Max Campos <mcampos2a@gmail.com>
2025-11-05 20:52:57 -0700

    commit daily kata

---
+++ parse-float.js
@@ -0,0 +1,24 @@
+//8 Kyu
+//Parse float
+//Fundamentals
+
+// Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.
+
+
+//Solution
+
+function parseF(s) {
+  console.log(s)
+  if(isNaN(parseFloat(s)) ){
+    return null
+  }else{
+    return parseFloat(s)
+  }
+}
+
+
+
+
+
+
+
