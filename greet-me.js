df3662ad60842f065adebe13bee9ebe99ea23e43
Max Campos <mcampos2a@gmail.com>
2025-11-13 20:18:07 -0700

    commit daily kata

---
+++ greet-me.js
@@ -0,0 +1,22 @@
+//7 Kyu
+//Greet Me
+//Fundamentals, strings
+
+// Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+// Example:
+
+// "riley" --> "Hello Riley!"
+// "JACK"  --> "Hello Jack!"
+
+
+//Solution
+
+
+var greet = function(name) {
+  return `Hello ${name[0].toUpperCase() + name.slice(1).toLowerCase()}!`
+};
+
+
+
+
